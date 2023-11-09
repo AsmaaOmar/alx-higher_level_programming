04ffb86f5b2d7ff8443f0a52cd68682ec17377dc
Asmaa <eng.asmaao2017@gmail.com>
2023-11-08 18:56:45 -0800

    p

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from task import add
+
+a = 1
+b = 2
+
+# This code should not run when this file is imported
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}". format(a, b, add(a, b)))
